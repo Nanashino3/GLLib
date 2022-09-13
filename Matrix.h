6e973d96b4745a5af7ae6eb1d4a56af6fd98f9dc
ta.kusumoto <ta.kusumoto11827@gmail.com>
2022-09-13 23:21:58 +0900

    ビュー行列を元の格納先に戻す、方向余弦の回転の計算が間違っていたので修正

--- Matrix.h
+++ Matrix.h
@@ -121,7 +121,6 @@ public:
 
 		GLfloat length = Vector3::Length(vec);
 		GLfloat dx = vec.mX / length, dy = vec.mY / length, dz = vec.mZ / length;
-
 		GLfloat cos = std::cosf(rotate), sin = std::sinf(rotate);
 
 		temp[0]  = dx * dx + (1.0f - dx * dx) * cos;
@@ -133,7 +132,7 @@ public:
 		temp[6]  = dy * dz * (1.0f - cos) - dx * sin;
 
 		temp[8]  = dz * dx * (1.0f - cos) - dy * sin;
-		temp[9]  = dz * dy * (1.0f - cos) + dz * sin;
+		temp[9]  = dz * dy * (1.0f - cos) + dx * sin;
 		temp[10] = dz * dz + (1.0f - dz * dz) * cos;
 
 		return temp;
@@ -154,11 +153,10 @@ public:
 
 		Matrix temp;
 		temp.LoadIdentity();
-		temp[0]  = xAxis.mX; temp[1]  = xAxis.mY; temp[2]  = xAxis.mZ;
-		temp[4]  = yAxis.mX; temp[5]  = yAxis.mY; temp[6]  = yAxis.mZ;
-		temp[8]  = zAxis.mX; temp[9]  = zAxis.mY; temp[10] = zAxis.mZ;
+		temp[0]  = xAxis.mX; temp[1]  = yAxis.mX; temp[2]  = zAxis.mX;
+		temp[4]  = xAxis.mY; temp[5]  = yAxis.mY; temp[6]  = zAxis.mY;
+		temp[8]  = xAxis.mZ; temp[9]  = yAxis.mZ; temp[10] = zAxis.mZ;
 		temp[12] = trans.mX; temp[13] = trans.mY; temp[14] = trans.mZ;
-
 		return temp;
 	}
 
